7b18a5412f13033fd0c6ab1bf56478a71120d34f
veb-bet <73333734+veb-bet@users.noreply.github.com>
2026-05-14 16:41:38 +0300

    Expand exports in __init__.py

--- vebmimesisify/__init__.py
+++ vebmimesisify/__init__.py
@@ -1,3 +1,19 @@
-from .scenarios import generate_scenario, generate_dialogue, generate_event_log
+from .scenarios import (
+    generate_scenario,
+    generate_dialogue,
+    generate_event_log,
+    generate_user_profile,
+    generate_blog_post,
+    generate_comment,
+    generate_web_event
+)
 
-__all__ = ["generate_scenario", "generate_dialogue", "generate_event_log"]
+__all__ = [
+    "generate_scenario",
+    "generate_dialogue",
+    "generate_event_log",
+    "generate_user_profile",
+    "generate_blog_post",
+    "generate_comment",
+    "generate_web_event"
+]
